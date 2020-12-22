74e87fa6b454dedf582398441825cc7eca62d3c1
Oleksandr Sierov <oleksandrsierov@gmail.com>
2020-12-22 16:04:37 +0000

    Version 0.2

---
+++ ClassLib/ClassLib/Utilizador.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca
+{
+    public class Utilizador
+    {
+        private int iduser;
+        private string username;
+        private int password;
+        private string email;
+        private byte estado;
+        private byte estadosessao;
+        private int pessoa_idpessoa;
+
+        /// <summary>
+        /// Apenas para testes
+        /// </summary>
+        public Utilizador()
+        {
+            iduser = -1;
+            username = "Ambrosio";
+            password = 123;
+            email = "abrosio2000@gmail.com";
+            estado = (byte)State.Ativo;
+            estadosessao = (byte)Sessao.Fechada;
+            pessoa_idpessoa = -1;
+        }
+
+        public Utilizador(string user, int pw, string email)
+        {
+            iduser = -1;
+            username = user;
+            password = pw;
+            this.email = email;
+            estado = (byte)State.Ativo;
+            estadosessao = (byte)Sessao.Fechada;
+            pessoa_idpessoa = -1;
+        }
+
+        public int Iduser
+        {
+            get
+            {
+                if (iduser != -1)
+                    return iduser;
+                else return -1;
+            }
+            set { if (iduser == -1) iduser = value; }
+        }
+
+        public int Pessoa_idpessoa
+        {
+            get
+            {
+                if (pessoa_idpessoa != -1)
+                    return pessoa_idpessoa;
+                else return -1;
+            }
+            set { if (pessoa_idpessoa == -1) pessoa_idpessoa = value; }
+        }
+
+        public string Username
+        {
+            get { return username; }
+        }
+        public int Password
+        {
+            get { return password; }
+        }
+        public string Email
+        {
+            get { return email; }
+        }
+        public byte Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+        public byte EstadoSessao
+        {
+            get { return estadosessao; }
+            set { estadosessao = value; }
+        }
+
+    }
+}
--- SOAPServicesWCF/SOAPServicesWCF/App_Code/GerirDB.cs
+++ SOAPServicesWCF/SOAPServicesWCF/App_Code/GerirDB.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Services;
 using Npgsql;
+using System.Configuration;
+using Biblioteca;
 
 /// <summary>
 /// Summary description for GerirDB
@@ -19,23 +21,63 @@ public class GerirDB : System.Web.Services.WebService {
         //Uncomment the following line if using designed components 
         //InitializeComponent(); 
     }
-    static string connstring = String.Format("Server={0};Port={1};" + "User Id={2};Password={3};Database={4};", "localhost", 5432, "postgres", 123, "aad");
-    private static NpgsqlConnection conn = new NpgsqlConnection(connstring);
+    
+    //static string connstring = String.Format("Server={0};Port={1};" + "User Id={2};Password={3};Database={4};", "localhost", 5432, "postgres", 123, "aad");
+    private static NpgsqlConnection conn= new NpgsqlConnection();
     private NpgsqlCommand cmd;
     private string sql = null;
 
+   // conn = new OleDbConnection(ConfigurationManager.ConnectionStrings["turismoConnectionString"].ConnectionString);
 
 
-    [WebMethod]
-    public string HelloWorld() {
 
+    /// <summary>
+    /// Metodo para registar o utilizador na base de dados
+    /// </summary>
+    /// <param name="u"></param>
+    /// <returns></returns>
+    [WebMethod(Description ="Registar Utilizador")]
+    public string RegistrarUser(Utilizador u,Pessoa p)
+    {
+        try
+        {
+            conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["PostGreConnectionString"].ConnectionString);
+            conn.Open();
+            sql = @"";
+            cmd = new NpgsqlCommand(sql, conn);
+            //cmd.Parameters.AddWithValue("parameter", parametervalue);
 
-        return "Hello World";
+            int result = (int)cmd.ExecuteScalar();
+            conn.Close();
+
+            if (result == 1)
+            {
+                return "Done";
+            }
+            else
+            {
+                return("Utilizador ja existe");
+            }
+
+        }
+        catch (Exception exception)
+        {
+            conn.Close();
+            return "Error"+exception.Message.ToString();
+            throw;
+        }
+        
     }
 
+
+    /// <summary>
+    /// TESTE CONEXAO BD
+    /// </summary>
+    /// <returns></returns>
     [WebMethod]
     public string Teste()
     {
+            conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["PostGreConnectionString"].ConnectionString);
             conn.Open();
             sql = @"select * from reg_insert(:email_doc,:password_doc,:nome_doc,:telemovel_doc)";
             cmd = new NpgsqlCommand(sql, conn);
