83d10e832792b3569b5067c8833ebfa449467689
Oleksandr Sierov <oleksandrsierov@gmail.com>
2020-12-27 15:40:44 +0000

    Version 1.1
    Add connstring to rest

---
+++ RESTServices/RESTServices/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace RESTServices.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IConfiguration _connfiguration;
+
+        private readonly string connStr;
+        private NpgsqlConnection conn;
+        string sql;
+
+
+
+        public UserController(IConfiguration configuration)
+        {
+            _connfiguration = configuration;
+            connStr = _connfiguration.GetConnectionString("PostGreConnectionString");
+            conn = new NpgsqlConnection(connStr);
+        }
+    }
+}
