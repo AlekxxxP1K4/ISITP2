8a6fa35726dbecc5d76d02262efd2976ea6cc617
Oleksandr Sierov <oleksandrsierov@gmail.com>
2020-12-27 16:02:35 +0000

    Version 1.2
    Login Rest

--- RESTServices/RESTServices/Controllers/UserController.cs
+++ RESTServices/RESTServices/Controllers/UserController.cs
@@ -8,14 +8,16 @@ using Npgsql;
 
 namespace RESTServices.Controllers
 {
-    public class UserController : Controller
+    [ApiController]
+    [Route("/user")]
+    public class UserController : ControllerBase
     {
         private readonly IConfiguration _connfiguration;
 
         private readonly string connStr;
         private NpgsqlConnection conn;
+        private NpgsqlCommand cmd;
         string sql;
-
 
 
         public UserController(IConfiguration configuration)
@@ -23,6 +25,31 @@ namespace RESTServices.Controllers
             _connfiguration = configuration;
             connStr = _connfiguration.GetConnectionString("PostGreConnectionString");
             conn = new NpgsqlConnection(connStr);
+        }
+
+        [HttpGet]
+        public int GetLogin(string user, string pw)
+        {
+            try
+            {
+                conn.Open();
+                sql = @"select * from Login_User(:_username,:_password)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_username", user);
+                cmd.Parameters.AddWithValue("_password", pw);
+
+                int result = (int)cmd.ExecuteScalar();
+
+                conn.Close();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return -3;
+                throw;
+            }
         }
     }
 }
